4bcf10f09befa0f2b8c2909c5199f3f36080ae1a
avidAves <74081439+avidAves@users.noreply.github.com>
2023-01-13 23:04:06 -0600

    Changed the snail and player to use rectangles!

--- 1st Pygame/test.py
+++ 1st Pygame/test.py
@@ -8,34 +8,32 @@ pygame.display.set_caption("Test Game Wahoo Yipee!")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("1st Pygame/font/Pixeltype.ttf",50)
 
-sky_surface = pygame.image.load('1st Pygame/graphics/Sky.png')
-ground_surface = pygame.image.load('1st Pygame/graphics/ground.png')
+sky_surface = pygame.image.load('1st Pygame/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('1st Pygame/graphics/ground.png').convert()
 text_surface = test_font.render("My game", False, "Black")
 
-snail_surface = pygame.image.load('1st Pygame/graphics/snail/snail1.png')
-#No need for a snail y position, since it doesn't move up and down
-snail_x_pos = 600
 
+snail_surface = pygame.image.load('1st Pygame/graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
+
+player_surf = pygame.image.load('1st Pygame/graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
-    #Checks for pygame events, lets the user close the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
     
-    #Displays the test surface
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    #Makes snail move to the left
-    snail_x_pos -= 6
-    #If snail moves off screen, this teleports it back to the right side, slightly off screen
-    if snail_x_pos <= -100:
-        snail_x_pos = 900
-    screen.blit(snail_surface,(snail_x_pos,250))
+    screen.blit(player_surf,player_rect)
+
+    if snail_rect.right < 0:
+        snail_rect.left = 800
+    snail_rect.left -= 4
+    screen.blit(snail_surface,snail_rect)
     #Updates the screen at a max rate of 60fps
 
     pygame.display.update()
     clock.tick(60)
-
-
